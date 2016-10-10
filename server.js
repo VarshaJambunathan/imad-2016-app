f33d8bb46b8a0ddfc2fe9fcbcd88d657a7b5a6a4
Varsha Jambunathan <jvarsha621@gmail.com>
2016-10-10 14:36:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,12 +141,9 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/article-two', function (req, res) {
-     res.send(createTemplate(articles[articleTwo]));
-});
-
-app.get('/article-three', function (req, res) {
-     res.send(createTemplate(articles[articleThree]));
+app.get('/:articleName', function (req, res) { 
+    var articleName = req.params.articleName; 
+    res.send(createTemplate(articles[articleName])); 
 });
 
 app.get('/ui/madi.png', function (req, res) {
