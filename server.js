6b6f64aab5e5ee004483dc680ecee21788002df7
Varsha Jambunathan <jvarsha621@gmail.com>
2016-10-14 13:16:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -138,10 +138,10 @@ app.get('/', function (req, res) {
 });
 
 var names=[];
-app.get('/submit-name/:name', function (req, res) {
+app.get('/submit-name', function (req, res) {
   //get the current name from the request object
   
-  var name=req.params.name;
+  var name=req.query.name;
   names.push(name);
   res.send(JSON.stringify(names));
 });
