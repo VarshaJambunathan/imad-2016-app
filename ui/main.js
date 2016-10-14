40e15e1128b744b28c6c5cb4518879bcab64486d
Varsha Jambunathan <jvarsha621@gmail.com>
2016-10-14 12:34:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,23 @@
-console.log('Loaded!');
+//Count of the times 'Click me ' is clicked'
+var button = document.getElementById('counter');
 
-//Change the text of the my-text div
-var element = document.getElementById('my-text');
-element.innerHTML= 'New Value' ;
-
-//Move the image
-var img = document.getElementById('madi');
-
-marginLeft=0;
-
-function moveRight () {
-    marginLeft=  marginLeft+ 5;
-    img.style.marginLeft= marginLeft + 'px'; 
-}
-
-img.onclick = function () {
-    var interval = setInterval(moveRight,50);
+button.onclick = function() {
+  // create a request
+  var request = new XMLHttpRequest();
+  
+  // capture the response and store it in a variable
+  reuest.onreadystatechange = function() {
+      if( request.readyState === XMLHttpRequest.DONE){
+        //do something  
+        if( request.status === 200){
+            var counter =  request.responseText();
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+      }
+      //request not served
+  };
+  //make a request to counter endpoint
+  request.open('GET','http://varshajambunathan.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
