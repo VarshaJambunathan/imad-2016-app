26179b2da529a880b6cfa2a1048b352d508f1dc5
Varsha Jambunathan <jvarsha621@gmail.com>
2016-10-14 11:44:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,8 @@ var element = document.getElementById('my-text');
 element.innerHTML= 'New Value' ;
 
 //Move the image
-var image = document.getElementById('madi');
-image.onClick = function () {
+var img = document.getElementById('madi');
+img.onClick = function () {
   
-  image.style.marginleft= '100px';  
+  img.style.marginleft= '100px';  
 };
