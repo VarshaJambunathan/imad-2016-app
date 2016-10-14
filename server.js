a96e33fb48460d9d25456100b4f2f08cc98d3320
Varsha Jambunathan <jvarsha621@gmail.com>
2016-10-14 13:44:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -137,18 +137,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter= 0; 
-app.get('/counter', function (req, res) {
-  counter = counter+1;
-  res.send(counter.toString());
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 var names=[];
@@ -160,13 +158,15 @@ app.get('/submit-name', function (req, res) {
   res.send(JSON.stringify(names));
 });
 
+var counter= 0; 
+app.get('/counter', function (req, res) {
+  counter = counter+1;
+  res.send(counter.toString());
+});
+
 app.get('/:articleName', function (req, res) { 
     var articleName = req.params.articleName; 
     res.send(createTemplate(articles[articleName])); 
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
