7f131e5fadc2fcb7c427030e8007ce9fa5bc1ec8
Varsha Jambunathan <jvarsha621@gmail.com>
2016-11-12 13:53:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/ui/style.cs`s', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
